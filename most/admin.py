889e4fb84a7da7ea31bc8699007661d3d635c095
beomjo <bbjj009911@gmail.com>
2025-10-02 11:31:41 +0900

    feat: 모델 생성 (#1)

--- most/admin.py
+++ most/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from most.models import MostMovie
 
 # Register your models here.
+@admin.register(MostMovie)
+class MostMovieAdmin(admin.ModelAdmin):
+    pass
--- most/models.py
+++ most/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class MostMovie(models.Model):
+    title = models.CharField("영화 제목", max_length=100)
+    genre = models.CharField("장르", max_length=100)
+    rating = models.FloatField("평점")
+    release_date = models.DateTimeField("출시 날짜")
+    male_rating = models.FloatField("남성 평점")
+    female_rating = models.FloatField("여성 평점")
