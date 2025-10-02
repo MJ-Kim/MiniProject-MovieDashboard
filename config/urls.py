0d5dca716a6fab13a448cc5b2436957e9b63ad3f
beomjo <bbjj009911@gmail.com>
2025-10-02 11:43:45 +0900

    feat: url patterns 정리

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("most/", include("most.urls")),
+    path("popular", include("popular.urls"))
 ]
